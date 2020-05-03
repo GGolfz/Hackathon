e3b4cfcf0301915523d2ea827cee6fc08785bcbd
Mr.WISARUT KITTICHAROENPHONNGAM <62130500226@st.sit.kmutt.ac.th>
2020-05-04 02:54:37 +0700

    [ADD] CSS

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tamaraidee.com Login Page</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <form method="POST" action="login.php">
