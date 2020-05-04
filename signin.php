8a19dde8c718ba2d4dc4b1a78ff74a75fd8ad566
Mr.WISARUT KITTICHAROENPHONNGAM <62130500226@st.sit.kmutt.ac.th>
2020-05-04 11:15:51 +0700

    add link

--- signin.php
+++ signin.php
@@ -223,8 +223,8 @@ button {
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="search">
-                <button class="btn navBut my-2 my-sm-0 m-1" type="submit">Login</button>
-                <button class="btn navBut my-2 my-sm-0 m-1" type="submit">Sign Up</button>
+                <a class="nav-link" href="#"> <button class="btn navBut my-2 my-sm-0 m-1" type="submit">Login</button></a>
+                <<a class="nav-link" href="signup.php"> button class="btn navBut my-2 my-sm-0 m-1" type="submit">Sign Up</button></a>
             </form>
         </div>
     </nav>
--- signup.php
+++ signup.php
@@ -84,8 +84,8 @@
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="search">
-                <button class="btn navBut my-2 my-sm-0 m-1" type="submit">Login</button>
-                <button class="btn navBut my-2 my-sm-0 m-1" type="submit">Sign Up</button>
+                <a class="nav-link" href="signin.php"> <button class="btn navBut my-2 my-sm-0 m-1" type="submit">Login</button></a>
+                <a class="nav-link" href="#"> <button class="btn navBut my-2 my-sm-0 m-1" type="submit">Sign Up</button></a>
             </form>
         </div>
     </nav>
